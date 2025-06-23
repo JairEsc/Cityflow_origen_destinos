0c8b0c9ef74b063c20473159ab3e3566a148b321
JairEsc <p.escobar816@gmail.com>
2025-06-23 13:16:46 -0600

    Más eficiente al leer solamente una vez cada .csv Se cuenta el número de viajes en mapa2 y se agrega como texto dinámico. Se agrega una capa de municipios de hidalgo. Los filtros responden como se esperaba

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 ##Idea: 
 
-En el .csv tenemos coordenada de inicio y de final de cada viaje. Estos viajes tienen la particularidad de terminar cerca (25m) de una troncal de tuzobus. 
+En el .csv tenemos coordenada de inicio y de final de cada viaje. En total son 22 millones de viajes con factor de expansión. Consideramos filtros para poder reducir el número de viajes por consulta. 
+E.g. Viajes en auto a medio día en diciembre del 15 al 22. 
 
+Cada filtro esta topado a 20k viajes.
+Supongamos una configuración de filtros dada por (A,B,C,D). 
+Se define una función fetch(a,b,c,d) que lee alguno de los archivos .csv y lo divide en dos objetos tipo Array,uno de origen y otro de destino. Incluyen representatividad por factor de expansión.
+
+
+Para cada configuración de filtros se definen los datos iniciales (20k), que abarcan todo el estado
 - Sea b la caja que contiene al mapa 1, la cual será variable.
-- Sea 1,2,...,n una numeración de los puntos en el mapa 1, y supongamoslos no-variables.
+- Sea 1,2,...,20k una numeración de los puntos en el mapa 1, y supongámoslos no-variables.
 
-El mapa 2 tiene puntos los puntos de destino del .csv, lo cuales están cerca (25m) de una cantidad pequeña de puntos fijos (+-20 troncales).  
-
-Al variar b, se hace un filtro de los viajes cuyos orígenes están dentro de b. Los viajes resultantes del filtro se pintan en el mapa 2 con sus destinos (troncales). 
-
-###Resumen
-El mapa(s) responde a la pregunta ¿A dónde van las personas que salen de aquí (mapa 1)?
+Se consideran los índices dentro de b y se agregan como un mapa de calor en el mapa2. 
+Ahora, si se consideran los datos dentro de los límites de map2, se hace un conteo del número de puntos y el número de viajes que representan, y se agrega como texto
--- app.js
+++ app.js
@@ -1,47 +1,34 @@
 
-var data;
-var data2;
+let data;
+let data2;
 let initial_bounds= [[20.118906, -98.741019], [20.118906, -98.741019]];
-
-function loadData(origen_o_destino = 'origen',mes='2023-06',time='evening',mode='driving',fecha_inicio=1,fecha_fin=30) {
+let filteredPoints = [];
+function loadData(mes='2023-06',time='evening',mode='driving',fecha_inicio=1,fecha_fin=30) {
     return new Promise((resolve, reject) => {
         Papa.parse('Rproj/Output/filtros/'+mes+'_'+time+'_'+mode+'.csv', {
             header: true,
             download: true,
             dynamicTyping: true,
-            complete: function(results) {
-                if(origen_o_destino === 'destino') {
+            complete: function(results) {//results.data son todos los renglones, incluidos coordenadas de origen, destino y pesos.
                     try {
                     const parsedData_or = (results.data).slice(0,-1).filter(function(row){
-                        const day = parseInt(row.start_timestamp.split('-')[2], 10);
+                        const day = parseInt(row.start_timestamp.split('-')[2], 10);//Filtro de calendario.
+                        //Los demás filtros se hacen a priori y se guardan en distintos csvs.
                         return day >= fecha_inicio && day <= fecha_fin;
-                    }).map(function(row) {
+                    }).map(function(row) {//
                         return [
                             parseFloat(row.overlap_origin_lat),
-                            parseFloat(row.overlap_origin_long)
+                            parseFloat(row.overlap_origin_long),
+                            parseFloat(row.overlap_destination_lat),
+                            parseFloat(row.overlap_destination_long),
+                            parseFloat(row.trip_scaled_ratio)
                         ];
                     });
                     resolve(parsedData_or);
                 } catch (err) {
                     reject(err);
                 }
-                }
-                else{
-                try {
-                    const parsedData = (results.data).slice(0,-1).filter(function(row){
-                        const day = parseInt(row.start_timestamp.split('-')[2], 10);
-                        return day >= fecha_inicio && day <= fecha_fin;
-                    }).map(function(row) {
-                        return [
-                            parseFloat(row.overlap_destination_lat),
-                            parseFloat(row.overlap_destination_long)
-                        ];
-                    });
-                    resolve(parsedData);
-                } catch (err) {
-                    reject(err);
-                }}
-                console.log(results.data);
+                //console.log(results.data);
             },
             error: function(err) {
                 reject(err);
@@ -58,27 +45,30 @@ const zoom = 13;
 // Initialize first map
 const map1 = L.map('map1').setView(center, zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors',
+    opacity: 0.6
 }).addTo(map1);
 
-// loadData(origen_o_destino='origen').then(parsedData => {
-//     data = parsedData;
-//     const heatPoints_l = L.heatLayer(data.slice(0, -1), { radius: 10, blur: 15, maxZoom: 1 });
-//     heatPoints_l.addTo(map1);
-// });
+// Add hidalgo polygons to map1 and map2
+function addHidalgoPolygon(map) {
+    L.geoJSON(hidalgo, {
+        style: {
+            color: 'white',
+            weight: 2,
+            fillColor: 'gray',
+            fillOpacity: 0.1,
+            zIndex: 1
+        }
+    }).addTo(map);
+}
+addHidalgoPolygon(map1);
 
-// const heatPoints_l = L.heatLayer(heatPoints, { radius: 10, blur: 15, maxZoom: 1 });
-// heatPoints_l.addTo(map1);
-// Initialize second map
 const map2 = L.map('map2').setView(center, zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors',
+    opacity: 0.6
 }).addTo(map2);
-// loadData(origen_o_destino='destino').then(parsedData => {
-//     data2 = parsedData;
-//     var heatPoints_ll = L.heatLayer(data2.slice(0, -1), { radius: 10, blur: 15, maxZoom: 1 });
-//     heatPoints_ll.addTo(map2);
-// });
+addHidalgoPolygon(map2);
 function handleSelectChange() {
     const mes = document.getElementById('mes').value;
     const time = document.getElementById('time').value;
@@ -90,60 +80,85 @@ function handleSelectChange() {
     const fecha_inicio = match ? parseInt(match[2], 10) : 1;
     const fecha_fin = match ? parseInt(match[3], 10) : 30;
     console.log('Selected values:', mes, time, mode, fecha_inicio, fecha_fin);
-    loadData('origen', mes, time, mode,fecha_inicio,fecha_fin).then(parsedData => {
-        data = parsedData;
+    loadData(mes, time, mode, fecha_inicio, fecha_fin).then(parsedData => {
+        // Store globally
+        data1 = [];
+        data2 = [];
+        parsedData.forEach(row => {
+            data1.push([row[0],row[1],row[4] ]);
+            data2.push([row[2], row[3], row[4] ]);
+        });
+
         map1.eachLayer(function(layer) {
             if (layer instanceof L.HeatLayer) {
                 map1.removeLayer(layer);
             }
         });
-        const heatPoints_l = L.heatLayer(data.slice(0, -1), { radius: 10, blur: 15, maxZoom: 1 });
+        const heatPoints_l = L.heatLayer(data1, { radius: 10, blur: 15, maxZoom: 1,max:100 });
         heatPoints_l.addTo(map1);
-        map1.fire('move');
-    });
-
-    loadData('destino', mes, time, mode,fecha_inicio,fecha_fin).then(parsedData => {
-        data2 = parsedData;
+        filteredPoints=data2
         map2.eachLayer(function(layer) {
             if (layer instanceof L.HeatLayer) {
                 map2.removeLayer(layer);
             }
         });
-        const heatPoints_ll = L.heatLayer(data2.slice(0, -1), { radius: 10, blur: 15, maxZoom: 1 });
+        const heatPoints_ll = L.heatLayer(data2.filter((coord,index)=>{
+            // coord should be [lat, lng, weight]
+            return map1.getBounds().contains([data1[index][0], data1[index][1]]);
+        }), { radius: 10, blur: 15, maxZoom: 1,max:100 });
         heatPoints_ll.addTo(map2);
     });
+
+
 }
 
-['mes', 'time', 'mode','daterange'].forEach(id => {
+['mes', 'time', 'mode'].forEach(id => {
     document.getElementById(id).addEventListener('change', function(e) {
         console.log(`Changed ${id}:`, e.target.value);
         handleSelectChange();
     });
 });
-handleSelectChange();
-
+handleSelectChange()
 
 map1.on('moveend', function() {
 //bounds que lo cubren
 //Checamos los origenes cuyos destinos están dentro de los límites del mapa
 //Recalcular pesos
 //Actualizar
-
+    
     const bounds = map1.getBounds();
+    initial_bounds=bounds
     //initial_bounds=bounds
     console.log('Bounds:', bounds);
+    //console.log('Initial data1:', data1);
     // Filter heatPoints_copy to keep only points inside map1 bounds
-    const filteredPoints = data2.slice(0,-1).filter(function(coord, index) {
+    filteredPoints = data2.slice(0,-1).filter(function(coord, index) {
         // coord should be [lat, lng, ...]
-        return bounds.contains([data.slice(0,-1)[index][0], data.slice(0,-1)[index][1]]);
+        return bounds.contains([data1.slice(0,-1)[index][0], data1.slice(0,-1)[index][1]]);
     });
-    console.log('Filtered Points:', filteredPoints);
+    //console.log('Filtered Points:', filteredPoints);
     // Remove old layer and add new filtered layer to map2
     map2.eachLayer(function(layer) {
         if (layer instanceof L.HeatLayer) {
             map2.removeLayer(layer);
         }
     });
-    heatPoints_ll = L.heatLayer(filteredPoints.map(([lat, lng]) => [lat, lng]), { radius: 10, blur: 15, maxZoom: 1 });
+    heatPoints_ll = L.heatLayer(filteredPoints, { radius: 10, blur: 15, maxZoom: 1,max:100 });
     heatPoints_ll.addTo(map2);
 });
+map2.on('moveend', function() {
+    const bounds = map2.getBounds();
+    //initial_bounds=bounds
+    //console.log('Bounds:', bounds);
+    
+    filteredPoints2 = filteredPoints.filter(function(coord, index) {
+        // coord should be [lat, lng, ...]
+        return bounds.contains([coord[0], coord[1]]);
+    
+
+    });
+    document.getElementById('texto').innerHTML = 
+        (filteredPoints2.length===1?'1 viaje único que representa':filteredPoints2.length+' viajes únicos que representan') + ' un total de ' + 
+        parseInt(filteredPoints2.reduce((a, b) => a + b[2], 0)) + 
+        ' viajes de acuerdo a su factor de expansión';
+})
